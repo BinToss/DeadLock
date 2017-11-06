b64153bba44c64c9e6e4cedd19377cbdc4f196a1
CodeDead <admin@codedead.com>
2017-11-06 20:10:03 +0100

    * Added Windows Explorer integration * Added Autorun functionality

--- DeadLock/Windows/SettingsWindow.xaml.cs
+++ DeadLock/Windows/SettingsWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 using DeadLock.Classes;
+using Microsoft.Win32;
 
 namespace DeadLock.Windows
 {
@@ -11,6 +14,8 @@ namespace DeadLock.Windows
     {
         #region Variables
         private readonly MainWindow _mw;
+        private bool _originalIntegration;
+        private bool _originalStartup;
         #endregion
 
         public SettingsWindow(MainWindow mainWindow)
@@ -31,6 +36,42 @@ namespace DeadLock.Windows
         }
 
         /// <summary>
+        /// Check if DeadLock starts automatically.
+        /// </summary>
+        /// <returns>A boolean to represent whether DeadLock starts automatically or not.</returns>
+        private static bool AutoStartUp()
+        {
+            return Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run", "DeadLock", "").ToString() == System.Reflection.Assembly.GetExecutingAssembly().Location;
+        }
+
+        /// <summary>
+        /// Check if Windows Explorer integration is enabled for the program.
+        /// </summary>
+        /// <returns>A boolean to represent whether Windows Explorer integration is enabled for the program.</returns>
+        private static bool WindowsExplorerIntegration()
+        {
+            bool fileExplorerIntegration = false;
+            bool folderExplorerIntegration = false;
+            using (RegistryKey key = Registry.ClassesRoot.OpenSubKey(@"*\shell\DeadLock\command"))
+            {
+                if (key != null && key.GetValue("", "").ToString() == System.Reflection.Assembly.GetExecutingAssembly().Location + " %1")
+                {
+                    fileExplorerIntegration = true;
+                }
+            }
+
+            using (RegistryKey key = Registry.ClassesRoot.OpenSubKey(@"Directory\shell\DeadLock\command"))
+            {
+                if (key != null && key.GetValue("", "").ToString() == System.Reflection.Assembly.GetExecutingAssembly().Location + " %1")
+                {
+                    folderExplorerIntegration = true;
+                }
+            }
+
+            return folderExplorerIntegration && fileExplorerIntegration;
+        }
+
+        /// <summary>
         /// Change the GUI elements to represent the latest settings
         /// </summary>
         private void LoadSettings()
@@ -48,6 +89,11 @@ namespace DeadLock.Windows
                 IntBorderThickness.Value = Properties.Settings.Default.BorderThickness;
 
                 ChbAutoOwnership.IsChecked = Properties.Settings.Default.AutoOwnership;
+
+                ChbAutoRun.IsChecked = AutoStartUp();
+                _originalStartup = (bool) ChbAutoRun.IsChecked;
+                ChbExplorerIntegration.IsChecked = WindowsExplorerIntegration();
+                _originalIntegration = (bool) ChbExplorerIntegration.IsChecked;
             }
             catch (Exception ex)
             {
@@ -60,6 +106,20 @@ namespace DeadLock.Windows
             if (MessageBox.Show("Are you sure that you want to reset all settings?", "DeadLock", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No) return;
             try
             {
+                List<string> args = new List<string>();
+                if (WindowsExplorerIntegration())
+                {
+                    args.Add("3");
+                }
+                if (AutoStartUp())
+                {
+                    args.Add("1");
+                }
+                if (args.Count != 0)
+                {
+                    StartRegManager(args);
+                }
+
                 Properties.Settings.Default.Reset();
                 Properties.Settings.Default.Save();
 
@@ -92,6 +152,24 @@ namespace DeadLock.Windows
                 if (IntBorderThickness.Value != null) Properties.Settings.Default.BorderThickness = (int)IntBorderThickness.Value;
                 if (ChbAutoOwnership.IsChecked != null) Properties.Settings.Default.AutoOwnership = (bool)ChbAutoOwnership.IsChecked;
 
+                List<string> args = new List<string>();
+                if (_originalStartup != (ChbAutoRun.IsChecked == true))
+                {
+                    _originalStartup = ChbAutoRun.IsChecked == true;
+                    args.Add(_originalStartup ? "0" : "1");
+                }
+
+                if (_originalIntegration != (ChbExplorerIntegration.IsChecked == true))
+                {
+                    _originalIntegration = ChbExplorerIntegration.IsChecked == true;
+                    args.Add(_originalIntegration ? "2" : "3");
+                }
+                if (args.Count != 0)
+                {
+                    args.Add("\"" + System.Reflection.Assembly.GetExecutingAssembly().Location + "\"");
+                    StartRegManager(args);
+                }
+
                 Properties.Settings.Default.Save();
 
                 LoadTheme();
@@ -104,6 +182,26 @@ namespace DeadLock.Windows
             {
                 MessageBox.Show(ex.Message, "DeadLock", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+        /// <summary>
+        /// Start the RegManager with a list of arguments.
+        /// </summary>
+        /// <param name="args">A list of arguments.</param>
+        private static void StartRegManager(IEnumerable<string> args)
+        {
+            string a = string.Join(" ", args);
+            Process process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = AppDomain.CurrentDomain.BaseDirectory + "\\RegManager.exe",
+                    Arguments = a,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                }
+            };
+            process.Start();
+            process.WaitForExit();
         }
     }
 }
